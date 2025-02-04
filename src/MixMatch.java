602c5a4412151b86d6fb7c9bf998e6651e998525
UrielAngeles09 <uriel.angeles01@utrgv.edu>
2025-02-04 02:31:02 +0000

    Lab 2 Done

---
+++ src/MixMatch.java
@@ -0,0 +1,35 @@
+public class MixMatch {
+    public static void main(String[] arg){
+        System.out.println("Must have a string " + 5 + true + 7.2);
+
+        System.out.println();
+        System.out.println("Mix & Match Data Types:");
+
+        String str = "str";
+        System.out.println(str + " Stores a sequence of characters");
+
+        char c = 'c';
+        System.out.println(c + " Stores a single character");
+
+        int x = 7;
+        System.out.println(x + " Is a 32-bit signed integer");
+
+        float y = 2.23f;
+        System.out.println(y + " Is a 32-bit floating-point number");
+
+        double z = 2.2323232323232d;
+        System.out.println(z + " Is a 64-bit floating-point number");
+
+        boolean  b = true;
+        System.out.println(b + " Is a Boolean value (true or false)");
+
+        long l = 9223372036854775807L;
+        System.out.println(l + " Is a 64-bit signed integer");
+
+        short s = 12345;
+        System.out.println(s + " Is a 16-bit signed integer");
+
+        // Outputting multiple variables with a single print method
+        System.out.println("Variables: " + str + ", " + c + ", " + x + ", " + y + ", " + z + ", " + b + ", " + l + ", " + s);
+    }
+} 
